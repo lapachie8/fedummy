edf3a5155cf910463f2b594fba7b14f1a4becb24
User <mayfaiq123@gmail.com>
2025-06-21 14:56:39 +0700

    first commit

---
+++ src/data/products.ts
@@ -0,0 +1,172 @@
+import { Product } from '../types';
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: 'abc',
+    description: 'aksesoris lengkap untuk bermain Honkai Star-Rail, termasuk controller, headset, dan mouse pad khusus.',
+    price: 75,
+    priceUnit: 'day',
+    category: 'Honkai Star-Rail',
+    imageUrl: 'https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 2,
+    name: 'apa',
+    description: 'asd',
+    price: 35,
+    priceUnit: 'day',
+    category: 'Honkai Star-Rail',
+    imageUrl: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 3,
+    name: 'ksjd',
+    description: 'asdd',
+    price: 40,
+    priceUnit: 'day',
+    category: 'Honkai Star-Rail',
+    imageUrl: 'https://images.pexels.com/photos/1117132/pexels-photo-1117132.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 4,
+    name: 'dsdzxc',
+    description: 'xzcw',
+    price: 25,
+    priceUnit: 'day',
+    category: 'Honkai Star-Rail',
+    imageUrl: 'https://images.pexels.com/photos/2582818/pexels-photo-2582818.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 5,
+    name: 'asds',
+    description: 'xvv',
+    price: 30,
+    priceUnit: 'day',
+    category: 'Honkai Star-Rail',
+    imageUrl: 'https://images.pexels.com/photos/1407322/pexels-photo-1407322.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 6,
+    name: 'xcqqw',
+    description: 'sdadd',
+    price: 20,
+    priceUnit: 'day',
+    category: 'Honkai Star-Rail',
+    imageUrl: 'https://images.pexels.com/photos/162553/keys-workshop-mechanic-tools-162553.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 7,
+    name: 'safg',
+    description: 'qffqf',
+    price: 45,
+    priceUnit: 'day',
+    category: 'Honkai Star-Rail Sports',
+    imageUrl: 'https://images.pexels.com/photos/1751550/pexels-photo-1751550.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 8,
+    name: 'saff',
+    description: 'asdd',
+    price: 120,
+    priceUnit: 'day',
+    category: 'Honkai Star-Rail',
+    imageUrl: 'https://images.pexels.com/photos/3784221/pexels-photo-3784221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 9,
+    name: 'asdasdsa',
+    description: 'saff',
+    price: 75,
+    priceUnit: 'day',
+    category: 'Anime',
+    imageUrl: 'https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 10,
+    name: 'asfasfs',
+    description: 'asfqqf',
+    price: 35,
+    priceUnit: 'day',
+    category: 'Anime',
+    imageUrl: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 11,
+    name: 'asqweq',
+    description: 'vvqweqe',
+    price: 40,
+    priceUnit: 'day',
+    category: 'Anime',
+    imageUrl: 'https://images.pexels.com/photos/1117132/pexels-photo-1117132.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 12,
+    name: 'xcvwqeq',
+    description: 'asfqwfwqwq',
+    price: 25,
+    priceUnit: 'day',
+    category: 'Anime',
+    imageUrl: 'https://images.pexels.com/photos/2582818/pexels-photo-2582818.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 13,
+    name: 'asdadasd',
+    description: 'xzveqqw',
+    price: 30,
+    priceUnit: 'day',
+    category: 'Anime',
+    imageUrl: 'https://images.pexels.com/photos/1407322/pexels-photo-1407322.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 14,
+    name: 'asdaxv',
+    description: 'asfqweq',
+    price: 20,
+    priceUnit: 'day',
+    category: 'Anime',
+    imageUrl: 'https://images.pexels.com/photos/162553/keys-workshop-mechanic-tools-162553.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 15,
+    name: 'safq',
+    description: 'xvqqw',
+    price: 45,
+    priceUnit: 'day',
+    category: 'Anime',
+    imageUrl: 'https://images.pexels.com/photos/1751550/pexels-photo-1751550.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: true
+  },
+  {
+    id: 16,
+    name: 'asdq',
+    description: 'asdada',
+    price: 120,
+    priceUnit: 'day',
+    category: 'Anime',
+    imageUrl: 'https://images.pexels.com/photos/3784221/pexels-photo-3784221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    available: false
+  }
+];
+
+export const categories = [
+  'All',
+  'Honkai Star-Rail',
+  'Anime',
+  'Honkai Impact 3rd',
+  'Genshin Impact'
+];
